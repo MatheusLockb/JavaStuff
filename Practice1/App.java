bf61140c7dc4e84cc7ecd47c044fc27979f1f486
Matheus Nascimento <144494501+MatheusLockb@users.noreply.github.com>
2024-10-31 18:26:40 -0300

    Update App.java

--- Practice1/App.java
+++ Practice1/App.java
@@ -1,1 +1,42 @@
 
+import java.util.ArrayList;
+import java.util.Scanner;
+    
+    public class App {
+        public static void main(String[] args) throws Exception {
+            ArrayList<Restaurante> ifood = new ArrayList<>();
+            Scanner input = new Scanner(System.in);
+            String nome;
+            String endereco;
+            double preco;
+            String tipoComida;
+    
+            System.out.println("--- Cadastro de 5 restaurantes ---");
+            for(int i = 0; i < 5; i++)
+            {
+                System.out.print("\t Nome: ");
+                nome = input.nextLine();
+                System.out.print("\t Endereço: ");
+                endereco = input.nextLine();
+                System.out.print("\t Preço: ");
+                preco = input.nextDouble();
+                tipoComida = input.nextLine();
+                System.out.print("\t Tipo comida: ");
+                tipoComida = input.nextLine();
+                Restaurante temporario = new Restaurante(nome, endereco, preco, tipoComida);
+                ifood.add(temporario);
+            }
+    
+            System.out.print("Digite o tipo de comida: ");
+            tipoComida = input.nextLine();
+    
+            for(Restaurante restaurante : ifood)
+            {
+                if(restaurante.getTipoComida().equals(tipoComida))
+                {
+                    restaurante.exibirDados();
+                }
+            }
+        }
+    }
+
